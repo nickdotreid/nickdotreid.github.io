6e1033353e748fcbe65ecb541c7dd8538e02f3e1
Nick Reid <nickreid@nickreid.com>
2021-10-28 19:27:43 -0700

    Created customized nickreid page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,29 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <>
+  <div class="content nickreid-logo">
+    <h1 class="nickreid-picture">
+      <em>Hello!</em>&nbsp;
+      My name is Nick Reid
+    </h1>
+    <p>
+      I'm a PhD student in <a href="http://bime.uw.edu/">
+        Biomedical Informatics and Medical Education
+      </a> at the University of Washington.
+      My advisor is Andrea Hartzler.
+    </p>
+    <p>
+      My research focuses on how support is provided to people navigating chronic health conditions.
+      I advocate for more accessible health information.
+    </p>
+    <p>
+      Aside from health information systems,
+      I enjoy bicycles and
+      napping in a hammock.
+    </p>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </>
 )
 
 export default IndexPage
