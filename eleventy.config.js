e8b8a9cc5738998992b2f3bb415f2012be3211e2
Nick Reid <nickreid@nickreid.com>
2024-06-01 13:53:29 -0700

    screwing around with css

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addGlobalData('layout', 'nickreid.njk');
-  eleventyConfig.addPassthroughCopy("nickreid.css");
+  eleventyConfig.addPassthroughCopy("./nickreid.css");
+  eleventyConfig.addWatchTarget("../nickreid.css");
 
   // Return your Object options:
   return {
