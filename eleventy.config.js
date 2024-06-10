bfc25c95b4c9efca14ae37b1151d6825a28e286a
Nick Reid <nickreid@nickreid.com>
2024-06-09 17:18:31 -0700

    Add transform to eleventy config

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,27 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./nickreid*");
   eleventyConfig.addWatchTarget("nickreid.css");
 
+  // Can be sync or async
+  eleventyConfig.addTransform("transform-name", async function (content) {
+    //console.log(this.page.inputPath);
+    console.log(this.page.outputPath);
+
+    regex = new RegExp(/\[\[(.*)\]\]/)
+    match = regex.exec(content);
+    while (match) {
+      console.log(match[0]);
+      content = match.input.substring(0, match.index)
+      + match[1].split("|")[0].trim()
+      + match.input.substring(
+        match.index + match[0].length,
+        match.input.length
+      );
+      match = regex.exec(content);
+    }
+    return content;
+  });
+
+
   // Return your Object options:
   return {
     dir: {
